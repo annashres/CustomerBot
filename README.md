8c65271ee7d8b4e5c5695e6935510547b2f13f5d
Ayo Olubeko <ayolubek@microsoft.com>
2017-04-25 07:11:35 -0700

    Adding conversation dialog to bot

--- README.md
+++ README.md
@@ -2,16 +2,50 @@
 
 This bot has been created using [Microsoft Bot Framework](https://dev.botframework.com), and scaffolded using the [Bot Builder Yeoman generator](https://github.com/GeekTrainer/generator-botbuilder).
 
-This bot is designed to record notes and insights from the conversations Microsoft employees have with customers. The goal is to make it easier to disseminate knowledge about Microsoft customers and win as a team.
+## Purpose
+MAAV-1 is designed to record notes and insights from the conversations Microsoft employees have with customers. The goal is to make it easier to disseminate knowledge about Microsoft customers and win as a team.
 
-[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+## Bot Deployment
+**Deploy bot on Azure**
+1. Make sure you have [Azure CLI 2.0](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli) installed and configured on your local machine. 
 
-## Getting Started on Azure
-Deploy MAAV-1, by clicking the 'Deploy to Azure' button above.
+2. Open a command prompt window in the project directory. Run the following commands to deploy MAAV-1 to a resource group called `ExampleBotDeploymentGroup` in Azure. *Note this will not work in PowerShell, make sure to run in command prompt window*
 
-## Getting Started on Local Machine
-When you clone locally, make to sure to create a '.env' in the root directory. This file will store the environment variables needed to run the bot.
-NODE_ENV = development
+```azurecli
+az login
+
+az group create --name ExampleBotDeploymentGroup --location "West US"
+az group deployment create \
+    --name MAAV1Deployment \
+    --resource-group ExampleBotDeploymentGroup \
+    --template-file azuredeploy.json \
+    --parameters @azuredeploy.parameters.json
+```
+
+3. Add project files and repo to Bot service on Azure.
+
+**Deploy bot on local machine**
+1. Make sure you have [node.js and npm](https://nodejs.org/en/) installed on your local machine. 
+
+2. Create a new file in the project directory called `.env`. This file will be used to setup environment variables that the bot will use to run.
+
+3. Copy the contents of the `.env.defaults` file into the `.env` file.
+
+4. Modify the `.env` file to add the following: `NODE_ENV = development`. This sets up your local machine as a dev environment.
+
+5. Open a command prompt window in the project directory. You can now start the bot locally by running the following command:
+
+```
+node app.js 
+```
+
+### Testing the bot
+**On Azure**
+Use the chat window built into the Azure bot service to send and receive messages from MAAV-1
+
+**On local machine**
+* Download the [bot framework emulator](https://download.botframework.com/bf-v3/tools/emulator/publish.htm)
+* Launch the emulator and use the chat window to send and receive messages from MAAV-1
 
 ### Structure
 
@@ -22,17 +56,6 @@ NODE_ENV = development
 `package.json` holds metadata relevant to the project including its list of dependencies.
 
 `azuredeploy.json` and `azuredeploy.parameters.json` hold the Azure Resource Manager template and configuration info needed to deploy the MAAV-1 bot to Azure.
-
-### Running the bot
-
-**Locally**
-
-```
-node app.js
-```
-
-**On Azure**
-Deploy the app and use the provided chat emulator to test out the bot
 
 
 ### Configuring the bot
--- bot.js
+++ bot.js
@@ -1,12 +1,20 @@
+"use strict";
 const builder = require('botbuilder');
+const azure_builder = require('botbuilder-azure');
 
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-const bot = new builder.UniversalBot(connector, [
-    (session, args, next) => {
+// Create chat bot
+var bot = new builder.UniversalBot(connector);
+
+//Add first run dialog
+bot.dialog('/firstRun',
+[
+    function (session)
+    {
         const botName = 'MAAV-1';
         const description = `I record notes and insights from the conversations Microsoft employees have with customers.  My purpose is to make it easier to disseminate knowledge about Microsoft customers and win as a team.`;
 
@@ -15,8 +23,69 @@ const bot = new builder.UniversalBot(connector, [
 
         builder.Prompts.text(session, `What's your name?`);
     },
-    (session, results, next) => {
-        session.endConversation(`Welcome, ${results.response}`);
+    function (session, results)
+    {
+        session.userData.name = results.response
+        session.endDialog("Nice to meet you %s. I'm now going to ask you questions to record your customer conversation ...", session.userData.name); 
+        session.beginDialog('/dataEntry');
+    }
+]);
+
+// Main dialog loop
+bot.dialog('/', [
+    function (session, args, next)
+    {
+        if (!session.userData.name)
+        {
+            session.beginDialog('/firstRun');
+        }
+        else
+        {
+            session.send("Hello there %s! Nice to see you again. I'm going to ask you questions now to record your customer conversation ...", session.userData.name);
+            session.beginDialog('/dataEntry');
+        }
+    }
+]);
+
+// Data entry dialog
+bot.dialog('/dataEntry',
+[
+    function (session)
+    {
+        builder.Prompts.text(session, "Besides yourself, who else was on the call");
+    },
+    function (session, results)
+    {
+        session.userData.authors = results.response;
+        builder.Prompts.text(session, "What company did y'all speak with?");
+    },
+    function (session, results)
+    {
+        session.userData.company = results.response;
+        builder.Prompts.text(session, "Who did you speak with at " + session.userData.company + "?");
+    },
+    function (session, results)
+    {
+        session.userData.contact = results.response;
+        builder.Prompts.choice(session, "Which products did you talk about?", ["SQLVM", "Elastic Pools", "Azure SQL DB", "On-Prem SQL Server", "Other"]);
+    },
+    function (session, results)
+    {
+        session.userData.product = results.response.entity;
+        builder.Prompts.text(session, "Please paste in below any notes you took down during the call");
+    },
+    function (session, results)
+    {
+        session.userData.notes = results.response;
+        builder.Prompts.text(session, "Any tags you would like to add to make this conversation easier to find in the future?");
+    },
+    function (session, results)
+    {
+        session.userData.tags = results.response;
+        var conversationNotes = JSON.stringify(session.userData);
+        session.send("That's all I need. Thanks for sending this info:");
+        session.send("%s", conversationNotes);
+        session.endDialog();
     }
 ]);
 
