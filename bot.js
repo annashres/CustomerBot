92cdaacba73c4ce65b40a79d9bc4f52acd38218b
Ayo Olubeko <ayolubek@microsoft.com>
2017-05-11 14:33:39 -0700

    Update environment variable names

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ var builder = require('botbuilder');
 var azure_builder = require('botbuilder-azure');
 
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Create chat bot
