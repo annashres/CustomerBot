bd1ff6c59d823903a0697409a7ec061525d71542
Ayo Olubeko <ayolubek@microsoft.com>
2017-05-27 12:45:25 -0700

    More debug options.

--- messages/bot.js
+++ messages/bot.js
@@ -539,8 +539,9 @@ bot.dialog('/batchParser',
             // Parse email signatures out of input text
             var templateTokens = args.replace(emailSignatureRegex, '');
             // Parse lines out of input text
-            templateTokens = templateTokens.replace(/---/g, '');
-            session.send(`Debug mode on:${templateTokens}`);
+            templateTokens = templateTokens.replace(/<hr>/g, '');
+            var debugText = JSON.stringify(templateTokens);
+            session.send(`Debug mode on:${debugText}`);
             
             templateTokens = templateTokens.split(conversationTemplateRegex);
 
